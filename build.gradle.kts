e38de05cbc5a45328410c14537beb2e6ce00c25d
Robin <rtyellowgrass@gmail.com>
2025-08-24 16:31:49 +0530

    Integrated sonarqube

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,18 @@ plugins {
     alias(libs.plugins.kotlinxSerialization) apply false
     alias(libs.plugins.kmpNativeCoroutines) apply false
     alias(libs.plugins.ksp) apply false
+
+    // ✅ Apply SonarQube plugin here (without `apply false`)
+    alias(libs.plugins.sonarqube)
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "kmp-sq_project")
+        property("sonar.organization", "kmp-sq")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.sourceEncoding", "UTF-8")
+        property("sonar.language", "kotlin") // ✅ Add this for KMP projects
+        property("sonar.sources", "src")    // ✅ Define sources if needed
+    }
 }
